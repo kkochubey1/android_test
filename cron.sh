f060b5a31d7df5d45ff2a8d3d2851e1bed54a76a
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-01-05 23:34:15 -0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,6 +1,11 @@
 set -ex
 
-echo "hi $(date +"%Y%m%d_%H%M%S")" >> /data/data/berserker.android.apps.sshdroid/home/hi.log
-curl --form file=@/data/data/berserker.android.apps.sshdroid/home/hi.log --form press=upload http://192.168.111.142:8000 &
+kill $(ps | grep curl | cut -f 1 -d " " ) || true
+
+mkdir -p /storage/external_SD/screenshots
+FILE_PATH=/storage/external_SD/screenshots/screenshot_$(date +"%Y%m%d_%H%M%S").png
+screencap -p $FILE_PATH && curl --form file=@$FILE_PATH --form press=upload http://192.168.111.142:8000 && rm @$FILE_PATH
 
 curl --form file=@/data/crontab/root.log --form press=upload http://192.168.111.142:8000 &
+
+find /storage/external_SD/screenshots/ -type f -print | while read -r file ; do echo "uploading $file"; curl --form file=@$file --form press=upload http://192.168.111.142:8000 && rm $file ; done
