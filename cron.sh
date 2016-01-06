24d9cc024cf35c65af068468f24d64d042ff7a86
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-01-05 22:32:08 -0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,1 +1,2 @@
 echo "hi $(date +"%Y%m%d_%H%M%S")" >> /data/data/berserker.android.apps.sshdroid/home/hi.log
+curl --form file=@/data/data/berserker.android.apps.sshdroid/home/hi.log --form press=upload http://192.168.111.142:8000
