d8acf1491c0765c75770e12f4fdb50eb54ecfc41
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-01-06 20:39:39 -0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -4,8 +4,9 @@ kill $(ps | grep curl | cut -f 1 -d " " ) || true
 
 mkdir -p /storage/external_SD/screenshots
 FILE_PATH=/storage/external_SD/screenshots/screenshot_$(date +"%Y%m%d_%H%M%S").png
-screencap -p $FILE_PATH && curl --form file=@$FILE_PATH --form press=upload http://192.168.111.142:8000 && rm @$FILE_PATH
+screencap -p $FILE_PATH && curl --form file=@$FILE_PATH --form press=upload http://192.168.111.142:8000
+rm @$FILE_PATH
 
 curl --form file=@/data/crontab/root.log --form press=upload http://192.168.111.142:8000 &
 
-find /storage/external_SD/screenshots/ -type f -print | while read -r file ; do echo "uploading $file"; curl --form file=@$file --form press=upload http://192.168.111.142:8000 && rm $file ; done
+find /storage/external_SD/screenshots/ -type f -print | while read -r file ; do echo "uploading $file"; curl --form file=@$file --form press=upload http://192.168.111.142:8000 ; rm $file ; done
