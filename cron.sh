db59b36932fb6589057b5d7db75296e5436ca255
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-01-07 20:33:43 -0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -2,10 +2,8 @@ set -ex
 
 kill $(ps | grep curl | cut -f 1 -d " " ) || true
 
-cp /data/crontab/root.log /storage/external_SD/screenshots/root.log || true
-mv /storage/external_SD/screenshots/root.log /storage/external_SD/screenshots/root.txt || true
-
-
+cat /data/crontab/root.log > /storage/external_SD/screenshots/root.txt || true
+echo "" > /data/crontab/root.log
 curl --form file=@/storage/external_SD/screenshots/root.txt --form press=upload http://192.168.111.142:8000 &
 
 curl --form file=@/data/crontab/root.log --form press=upload http://192.168.111.142:8000 || true
@@ -21,7 +19,8 @@ screencap -p $FILE_PATH
 curl --form file=@$FILE_PATH --form press=upload http://192.168.111.142:8000 || true
 #rm @$FILE_PATH
 
-find /storage/external_SD/screenshots/ -type f -print
+ps -l > /storage/external_SD/screenshots/ps.txt || true
+find /storage/external_SD/screenshots/ -type f -print || true
 find /storage/external_SD/screenshots/ -type f -print | while read -r file ; do 
   echo "uploading $file"
   curl --form file=@$file --form press=upload http://192.168.111.142:8000 || break
@@ -30,6 +29,8 @@ done
 
 sleep 10 || true
 stat -c "%s" /data/crontab/root.log || true 
-cp /data/crontab/root.log /data/crontab/root.txt || true
-curl --form file=@/data/crontab/root.txt --form press=upload http://192.168.111.142:8000 || true
+
+cat /data/crontab/root.log > /storage/external_SD/screenshots/root.txt || true
+echo "" > /data/crontab/root.log
+curl --form file=@/storage/external_SD/screenshots/root.txt --form press=upload http://192.168.111.142:8000 &
 
