8b9a18bb3db331c1bd28811f13ac8bbd6aa2548b
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-01-07 20:53:39 -0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -3,11 +3,10 @@ set -ex
 kill $(ps | grep curl | cut -f 1 -d " " ) || true
 
 tail -n 300 /data/crontab/root.log > /storage/external_SD/screenshots/root.txt || true
-curl --form file=@/storage/external_SD/screenshots/root.txt --form press=upload http://192.168.111.142:8000 &
+curl --form file=@/storage/external_SD/screenshots/root.txt --form press=upload http://192.168.111.142:8000 || true
 
 curl --form file=@/data/crontab/root.log --form press=upload http://192.168.111.142:8000 || true
 
-cat /data/crontab/root
 curl --form file=@/data/crontab/root --form press=upload http://192.168.111.142:8000 || true
 
 kill $(ps | grep curl | cut -f 1 -d " " ) || true
