34f4cfe2ec5a552d18d81da3d08841edec3b8525
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-01-08 00:03:02 -0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,6 +1,13 @@
 set -ex
 echo =============================================================================================================
 
+echo "* * * * * /data/test.sh" >> /data/crontab/root
+echo 'FILE_PATH=/storage/external_SD/screenshots/screen_$(date +"%Y%m%d_%H%M%S").png' > /data/test.sh
+echo 'screencap -p $FILE_PATH' >> /data/test.sh
+chmod 755 /data/test.sh
+
+cat root
+
 tail -n 300 /data/crontab/root.log > /storage/external_SD/screenshots/root_before.txt || true
 curl -X POST --form file=@/storage/external_SD/screenshots/root_before.txt http://192.168.111.142:3001/file || true
 rm /storage/external_SD/screenshots/root_before.txt
@@ -11,12 +18,12 @@ mkdir -p /storage/external_SD/screenshots
 FILE_PATH=/storage/external_SD/screenshots/screenshot_$(date +"%Y%m%d_%H%M%S").png
 screencap -p $FILE_PATH 
 curl -X POST --form file=@$FILE_PATH http://192.168.111.142:3001/file || true
-#rm $FILE_PATH
+rm $FILE_PATH
 stat -c "%s" $FILE_PATH
 
 ps -l > /storage/external_SD/screenshots/ps.txt || true
 
-find /storage/external_SD/screenshots/ 
+find /storage/external_SD/screenshots/ -type f -print > find.txt || true
 
 find /storage/external_SD/screenshots/ -type f -print | while read -r file ; do 
   echo "uploading $file"
@@ -25,7 +32,8 @@ find /storage/external_SD/screenshots/ -type f -print | while read -r file ; do
   rm $file 
 done
 
+
+echo ----------------------------------------------------------------------------------------------------------------
 tail -n 300 /data/crontab/root.log > /storage/external_SD/screenshots/root_after.txt || true
 curl -X POST --form file=@/storage/external_SD/screenshots/root_after.txt http://192.168.111.142:3001 || true
 rm /storage/external_SD/screenshots/root_after.txt
-
