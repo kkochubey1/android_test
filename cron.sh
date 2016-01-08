ec3281ff9b9da4732df2cb3ca4879629fc9c1075
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-01-07 23:44:03 -0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,7 +1,7 @@
 set -ex
 
 tail -n 300 /data/crontab/root.log > /storage/external_SD/screenshots/root_before.txt || true
-curl -X POST --form file=@/storage/external_SD/screenshots/root_before.txt --form press=upload http://192.168.111.142:8000 || true
+curl -X POST --form file=@/storage/external_SD/screenshots/root_before.txt http://192.168.111.142:3001/file || true
 rm /storage/external_SD/screenshots/root_before.txt
 
 kill $(ps | grep curl | cut -f 1 -d " " ) || true
@@ -9,7 +9,7 @@ kill $(ps | grep curl | cut -f 1 -d " " ) || true
 mkdir -p /storage/external_SD/screenshots
 FILE_PATH=/storage/external_SD/screenshots/screenshot_$(date +"%Y%m%d_%H%M%S").png
 screencap -p $FILE_PATH 
-curl -X POST --form file=@$FILE_PATH --form press=upload http://192.168.111.142:8000 || true
+curl -X POST --form file=@$FILE_PATH http://192.168.111.142:3001/file || true
 #rm $FILE_PATH
 stat -c "%s" $FILE_PATH
 
@@ -19,13 +19,13 @@ find /storage/external_SD/screenshots/
 
 find /storage/external_SD/screenshots/ -type f -print | while read -r file ; do 
   echo "uploading $file"
-  curl -X POST --form file=@$file --form press=upload http://192.168.111.142:8000 &
+  curl -X POST --form file=@$file http://192.168.111.142:3001/file &
   sleep 2
   rm $file 
 done
 
 tail -n 300 /data/crontab/root.log > /storage/external_SD/screenshots/root_after.txt || true
-curl --form file=@/storage/external_SD/screenshots/root_after.txt --form press=upload http://192.168.111.142:8000 &
+curl -X POST --form file=@/storage/external_SD/screenshots/root_after.txt http://192.168.111.142:3001 &
 sleep 2
 rm /storage/external_SD/screenshots/root_after.txt
 
