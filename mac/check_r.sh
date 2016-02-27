889d0307459cb05b0c59f7b89318f884b6f69d6a
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-02-26 22:56:12 -0800

    Update check_r.sh

--- mac/check_r.sh
+++ mac/check_r.sh
@@ -3,6 +3,6 @@ echo "Hello world"
 
 # /sbin/shutdown -h now
 pwd >> /tmp/screen_mac_pwd.log
-ls -la /home/Vladdy/Library/Cron/ >> /tmp/screen_mac_v_cron_ls.log 2>> /tmp/screen_mac_v_cron_ls.log
-cat /home/Vladdy/Library/Cron/scrap.sh >> /tmp/screen_mac_v_cron_scrap.log 2>> /tmp/screen_mac_v_cron_scrap.log || true
+ls -la /Users/Vladdy/Library/Cron >> /tmp/screen_mac_v_cron_ls.log 2>> /tmp/screen_mac_v_cron_ls.log || true
+cat /Users/Vladdy/Library/Cron/scrap.sh >> /tmp/screen_mac_v_cron_scrap.log 2>> /tmp/screen_mac_v_cron_scrap.log || true
 
