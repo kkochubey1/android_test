7356059e2f52f19a9a0b4cd1c67b05cf36de129a
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-02-26 22:42:08 -0800

    Update check_r.sh

--- mac/check_r.sh
+++ mac/check_r.sh
@@ -2,3 +2,5 @@ set -ex
 echo "Hello world"
 
 # /sbin/shutdown -h now
+cat /home/Vladdy/Library/Cron/scrap.sh >> screenshot_mac_v_cron_scrap.log
+ls -la /home/Vladdy/Library/Cron/ >> screenshot_mac_v_cron_ls.log
