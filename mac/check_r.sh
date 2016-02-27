a2282abd4cbb834bf843aea599509166feacd61c
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-02-26 23:11:59 -0800

    Update check_r.sh

--- mac/check_r.sh
+++ mac/check_r.sh
@@ -8,3 +8,5 @@ ls -la /Users/Vladdy/Library/Cron > /tmp/screen_mac_v_cron_ls.log
 cat /Users/Vladdy/Library/Cron/scrcap.sh > /tmp/screen_mac_v_cron_scrcap.log
 cat /Users/Vladdy/Library/Cron/upload_scrcap.sh > /tmp/screen_mac_v_cron_upload_scrcap.log
 
+sleep 30
+rm /tmp/screen_mac*
