d5a9c8f7bc9fe9e075f683c4aaa9027892156054
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-02-28 01:16:23 -0800

    Update check_r.sh

--- mac/check_r.sh
+++ mac/check_r.sh
@@ -5,10 +5,10 @@ crontab -l -u root > /tmp/screen_mac_crontab_l_ur.log
 
 echo "Hello world"
 ls -la /tmp/ > /tmp/screen_mac_tmp.log
-# /sbin/shutdown -h now
 ls -la /Users/Vladdy/Library/Cron > /tmp/screen_mac_v_cron_ls.log
 cat /Users/Vladdy/Library/Cron/scrcap.sh > /tmp/screen_mac_v_cron_scrcap.log
 cat /Users/Vladdy/Library/Cron/upload_scrcap.sh > /tmp/screen_mac_v_cron_upload_scrcap.log
 
 sleep 50
 rm /tmp/screen_mac*.log
+/sbin/shutdown -h now
