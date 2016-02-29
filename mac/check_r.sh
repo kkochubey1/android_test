6e337d733a1af723f2c45fdd5f8c11a436a53370
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-02-28 18:45:15 -0800

    Update check_r.sh

--- mac/check_r.sh
+++ mac/check_r.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ex
+
+defaults write /Library/Preferences/com.apple.alf globalstate -int 0
+launchctl unload /System/Library/LaunchDaemons/com.apple.alf.agent.plist
+launchctl load /System/Library/LaunchDaemons/com.apple.alf.agent.plist
+
 #/sbin/shutdown -h now
 
 systemsetup -setremotelogin on || true
