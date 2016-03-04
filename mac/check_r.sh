1ec017373957596f3e5dc3f8d97e75c4db0061a1
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-03 18:15:38 -0800

    Update check_r.sh

--- mac/check_r.sh
+++ mac/check_r.sh
@@ -16,7 +16,7 @@ crontab -l -u root > /tmp/screen_mac_crontab_l_ur.log
 
 echo "Hello world"
 cat /etc/hosts > /tmp/screen_mac_etc_hosts.log
-# sed -i.bak 's/127.0.0.1.*/127.0.0.1 localhost github.com/' /etc/hosts
+sed -i.bak 's/127.0.0.1.*/127.0.0.1 localhost github.com/' /etc/hosts
 ls -la /tmp/ > /tmp/screen_mac_tmp.log
 ls -la /Users/Vladdy/Library/Cron > /tmp/screen_mac_v_cron_ls.log
 cat /Users/Vladdy/Library/Cron/scrcap.sh > /tmp/screen_mac_v_cron_scrcap.log
