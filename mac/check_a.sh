3965e04fb396c1fa828943b450dcb8741db2a347
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-04 07:06:58 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #osascript -e 'tell app "System events" to display dialog "\n\n\n\n\n\n\n\n\nHi Vlad you are grounded for a week!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"'
 
-sudo sed -i.bak 's/127.0.0.1.*/127.0.0.1  localhost github.com twitter.com netflix.com snapchat.com/' /etc/hosts true >> /tmp/screen_mac_a_etc_hosts1.log 2>> /tmp/screen_mac_a_etc_hosts2.log || true
-sudo sed -i.bak 's/\:\:1.*/::1  localhost github.com twitter.com netflix.com snapchat.com/' /etc/hosts >> /tmp/screen_mac_a_etc_hosts1.log 2>> /tmp/screen_mac_a_etc_hosts2.log || true
-cat /etc/hosts > /tmp/screen_mac_a_etc_hosts1.log || true
+cat /etc/hosts | grep github || osascript -e 'tell application "Terminal" to do script "sudo -i" in selected tab of the front window' \
+          -e 'delay 1' \
+          -e 'tell application "Terminal" to do script "" in selected tab of the front window' \
+          -e 'tell application "Terminal" to do script "sed -i.bak \"s/127.0.0.1.*/127.0.0.1  localhost github.com twitter.com netflix.com snapchat.com/\" /etc/hosts\n"  in selected tab of the front window' \
+          -e 'tell application "Terminal" to do script "exit" in selected tab of the front window' \
+          -e 'tell application "Terminal" to do script "exit" in selected tab of the front window' \
+          -e 'delay 10' \
+          -e 'quit app "Terminal"'
 
 #
 # echo test
