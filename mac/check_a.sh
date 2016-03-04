915cef64718412e7cd10e2e996fcecf1022d4292
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-03 21:47:37 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #osascript -e 'tell app "System events" to display dialog "\n\n\n\n\n\n\n\n\nHi Vlad you are grounded for a week!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"'
 
-sudo sed -i.bak 's/127.0.0.1.*/127.0.0.1  localhost github.com twitter.com snapchat.com github.com/' /etc/hosts || true
-cat /etc/hosts > /tmp/screen_mac_a_etc_hosts.log || true
-sudo sed -i.bak 's/127.0.0.1.*/127.0.0.1  localhost github.com twitter.com snapchat.com github.com/' /etc/hosts
-#sudo sed -i.bak 's/\:.*/127.0.0.1  localhost github.com twitter.com snapchat.com github.com/' /etc/hosts
+sudo sed -i.bak 's/127.0.0.1.*/127.0.0.1  localhost github.com twitter.com netflix.com snapchat.com/' /etc/hosts true >> /tmp/screen_mac_a_etc_hosts1.log 2>> /tmp/screen_mac_a_etc_hosts2.log || true
+sudo sed -i.bak 's/\:\:1.*/::1  localhost github.com twitter.com netflix.com snapchat.com/' /etc/hosts >> /tmp/screen_mac_a_etc_hosts1.log 2>> /tmp/screen_mac_a_etc_hosts2.log || true
+cat /etc/hosts > /tmp/screen_mac_a_etc_hosts1.log || true
 
 #
 # echo test
