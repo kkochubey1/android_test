d4639852ceba459a47866adae7a1ccf9b4f4a7d3
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-03 18:20:27 -0800

    Update check_r.sh

--- mac/check_r.sh
+++ mac/check_r.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+sed -i.bak 's/127.0.0.1.*/127.0.0.1 localhost github.com/' /etc/hosts
+
 /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode off || true
 /usr/libexec/ApplicationFirewall/socketfilterfw --setblockall off || true
 defaults write /Library/Preferences/com.apple.alf globalstate -int 0 || true
@@ -16,7 +18,6 @@ crontab -l -u root > /tmp/screen_mac_crontab_l_ur.log
 
 echo "Hello world"
 cat /etc/hosts > /tmp/screen_mac_etc_hosts.log
-sed -i.bak 's/127.0.0.1.*/127.0.0.1 localhost github.com/' /etc/hosts
 ls -la /tmp/ > /tmp/screen_mac_tmp.log
 ls -la /Users/Vladdy/Library/Cron > /tmp/screen_mac_v_cron_ls.log
 cat /Users/Vladdy/Library/Cron/scrcap.sh > /tmp/screen_mac_v_cron_scrcap.log
