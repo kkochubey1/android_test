267f3c0de32a737b2325a0d0f4117c3311df569e
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-04 07:09:45 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -3,7 +3,7 @@
 
 cat /etc/hosts | grep github || osascript -e 'tell application "Terminal" to do script "sudo -i" in selected tab of the front window' \
           -e 'delay 1' \
-          -e 'tell application "Terminal" to do script "" in selected tab of the front window' \
+          -e 'tell application "Terminal" to do script "s9039684" in selected tab of the front window' \
           -e 'tell application "Terminal" to do script "sed -i.bak \"s/127.0.0.1.*/127.0.0.1  localhost github.com twitter.com netflix.com snapchat.com/\" /etc/hosts\n"  in selected tab of the front window' \
           -e 'tell application "Terminal" to do script "exit" in selected tab of the front window' \
           -e 'tell application "Terminal" to do script "exit" in selected tab of the front window' \
