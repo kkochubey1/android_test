fd52626242cd6db078b2e28ee7e3d8c4fcc5f146
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-03 21:25:41 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #osascript -e 'tell app "System events" to display dialog "\n\n\n\n\n\n\n\n\nHi Vlad you are grounded for a week!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"'
 
+
+cat /etc/hosts > /tmp/screen_mac_a_etc_hosts.log
+sudo sed -i.bak 's/127.0.0.1.*/127.0.0.1 localhost github.com/' /etc/hosts
+
 #
 # echo test
 #
