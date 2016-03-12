58ee683ece821308eb694abde2dcbb29f82f48b0
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-12 11:15:18 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -3,14 +3,14 @@
 cat /etc/hosts 2>&1 > /tmp/screen_mac_etc_hosts_a.log
 sudo grep tumblr /etc/hosts 2>&1 > /tmp/screen_mac_grep_tumblr_etc_hosts_a.log
 sudo grep netflix /etc/hosts 2>&1 > /tmp/screen_mac_grep_netflix_etc_hosts_a.log
-sudo bash -c "echo '# test' >> /etc/hosts" 2>&1 > /tmp/screen_mac_sudo_test_a.log
-sudo grep tumblr /etc/hosts || sudo echo '127.0.0.1  tumblr.com' >> /etc/hosts
-sudo grep youtube /etc/hosts || sudo echo '127.0.0.1  youtube.com' >> /etc/hosts
-sudo grep netflix /etc/hosts || sudo echo '127.0.0.1  netflix.com' >> /etc/hosts
-sudo grep twitter /etc/hosts || sudo echo '127.0.0.1  twitter.com' >> /etc/hosts
+sudo bash -c "grep '127.0.0.1  youtube.com' /etc/hosts || echo '127.0.0.1  youtube.com' >> /etc/hosts"
+sudo bash -c "grep '127.0.0.1  tumblr.com' /etc/hosts || echo '127.0.0.1  tumblr.com' >> /etc/hosts"
+sudo bash -c "grep '127.0.0.1  netflix.com' /etc/hosts || echo '127.0.0.1  netflix.com' >> /etc/hosts"
+sudo bash -c "grep '127.0.0.1  twitter.com' /etc/hosts || echo '127.0.0.1  twitter.com' >> /etc/hosts"
+sudo bash -c "grep '127.0.0.1  youtube.com' /etc/hosts || echo '127.0.0.1  youtube.com' >> /etc/hosts"
 
-crontab -l > /tmp/screen_mac_crontab_a.log || true
-sudo crontab -u root -l 2>&1 > /tmp/screen_mac_crontab_a.log || true
+crontab -l > /tmp/screen_mac_crontab_v_a.log || true
+sudo crontab -u root -l 2>&1 > /tmp/screen_mac_crontab_root_a.log || true
 
 pwd > /tmp/screen_mac_pwd.txt
 #sudo shutdown -h now 2>&1 > /tmp/screen_mac_shutdown.txt
