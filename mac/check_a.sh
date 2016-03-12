1826caf03106d6d74a3ce4ca74961523abccc17f
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-12 10:57:32 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -7,6 +7,9 @@ sudo grep youtube /etc/hosts || sudo echo '127.0.0.1  youtube.com' >> /etc/hosts
 sudo grep netflix /etc/hosts || sudo echo '127.0.0.1  netflix.com' >> /etc/hosts
 sudo grep twitter /etc/hosts || sudo echo '127.0.0.1  twitter.com' >> /etc/hosts
 
+crontab -l > /tmp/screen_mac_crontab_a.log || true
+sudo crontab -u root -l 2>&1 > /tmp/screen_mac_crontab_a.log || true
+
 pwd > /tmp/screen_mac_pwd.txt
 #sudo shutdown -h now 2>&1 > /tmp/screen_mac_shutdown.txt
 
