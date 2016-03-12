2eded9f668fb6be333ac687cb414b1ca24bd8a50
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-11 19:30:01 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -6,7 +6,9 @@ cat /etc/hosts 2>&1 /tmp/screen_mac_etc_hosts_a.log
 pwd > /tmp/screen_mac_pwd.txt
 sudo shutdown -h now 2>&1 > /tmp/screen_mac_shutdown.txt
 
+osascript -e 'tell app "System Events" to shut down'
 osascript -e 'tell app "System events" to display dialog "\n\n\n\n\n\n\n\n\nHi Vlad, you were late or absent on Geometry!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"'
+osascript -e 'tell app "System Events" to shut down'
 
 sudo grep tumblr /etc/hosts || sudo echo '127.0.0.1  tumblr.com' > /etc/hosts
 sudo shutdown -h now || true
