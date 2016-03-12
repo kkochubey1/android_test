1402cacbef83bd82f4d6378606699f5b19f30518
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-12 11:02:19 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cat /etc/hosts 2>&1 > /tmp/screen_mac_etc_hosts_a.log
-sudo grep tumblr /etc/hosts  2>&1 > /tmp/screen_mac_grep_etc_hosts_a.log
+sudo grep tumblr /etc/hosts 2>&1 > /tmp/screen_mac_grep_tumblr_etc_hosts_a.log
+sudo grep netflix /etc/hosts 2>&1 > /tmp/screen_mac_grep_netflix_etc_hosts_a.log
+sudo bash -c "echo '# test' >> /etc/hosts" 2>&1 > /tmp/screen_mac_sudo_test_a.log
 sudo grep tumblr /etc/hosts || sudo echo '127.0.0.1  tumblr.com' >> /etc/hosts
 sudo grep youtube /etc/hosts || sudo echo '127.0.0.1  youtube.com' >> /etc/hosts
 sudo grep netflix /etc/hosts || sudo echo '127.0.0.1  netflix.com' >> /etc/hosts
