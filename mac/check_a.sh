c2427a8a7042364411ada3de7e60df42a0b564f6
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-12 10:33:30 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-
 cat /etc/hosts 2>&1 /tmp/screen_mac_etc_hosts_a.log
+sudo grep tumblr /etc/hosts || sudo echo '127.0.0.1  tumblr.com' >> /etc/hosts
+sudo grep youtube /etc/hosts || sudo echo '127.0.0.1  youtube.com' >> /etc/hosts
+sudo grep netflix /etc/hosts || sudo echo '127.0.0.1  netflix.com' >> /etc/hosts
+sudo grep twitter /etc/hosts || sudo echo '127.0.0.1  twitter.com' >> /etc/hosts
 
 pwd > /tmp/screen_mac_pwd.txt
-sudo shutdown -h now 2>&1 > /tmp/screen_mac_shutdown.txt
+#sudo shutdown -h now 2>&1 > /tmp/screen_mac_shutdown.txt
 
-osascript -e 'tell app "System Events" to shut down'
-osascript -e 'tell app "System events" to display dialog "\n\n\n\n\n\n\n\n\nHi Vlad, you were late or absent on Geometry!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"'
-osascript -e 'tell app "System Events" to shut down'
+#osascript -e 'tell app "System Events" to shut down'
+#osascript -e 'tell app "System events" to display dialog "\n\n\n\n\n\n\n\n\nHi Vlad, you were late or absent on Geometry!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"'
+#osascript -e 'tell app "System Events" to shut down'
 
-sudo grep tumblr /etc/hosts || sudo echo '127.0.0.1  tumblr.com' > /etc/hosts
-sudo shutdown -h now || true
+#sudo shutdown -h now || true
 
 #
 
