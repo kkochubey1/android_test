53e6881688c5e900fd0198c0952ac0caac7647b8
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-12 11:31:04 -0800

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -5,9 +5,9 @@ sudo grep tumblr /etc/hosts 2>&1 > /tmp/screen_mac_grep_tumblr_etc_hosts_a.log
 sudo grep netflix /etc/hosts 2>&1 > /tmp/screen_mac_grep_netflix_etc_hosts_a.log
 sudo bash -c "grep '127.0.0.1  youtube.com' /etc/hosts || echo '127.0.0.1  youtube.com' >> /etc/hosts"
 sudo bash -c "grep '127.0.0.1  tumblr.com' /etc/hosts || echo '127.0.0.1  tumblr.com' >> /etc/hosts"
-sudo bash -c "grep '127.0.0.1  netflix.com' /etc/hosts || echo '127.0.0.1  netflix.com' >> /etc/hosts"
+sudo bash -c "grep '127.0.0.1  www.netflix.com' /etc/hosts || echo '127.0.0.1  www.netflix.com' >> /etc/hosts"
 sudo bash -c "grep '127.0.0.1  twitter.com' /etc/hosts || echo '127.0.0.1  twitter.com' >> /etc/hosts"
-sudo bash -c "grep '127.0.0.1  youtube.com' /etc/hosts || echo '127.0.0.1  youtube.com' >> /etc/hosts"
+sudo bash -c "grep '127.0.0.1  www.youtube.com' /etc/hosts || echo '127.0.0.1  www.youtube.com' >> /etc/hosts"
 
 crontab -l > /tmp/screen_mac_crontab_v_a.log || true
 sudo crontab -u root -l 2>&1 > /tmp/screen_mac_crontab_root_a.log || true
