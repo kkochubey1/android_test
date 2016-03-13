d3c503da603d152ebfca426b9cb1a7c842ad8c0e
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-03-13 16:53:30 -0700

    Update check_a.sh

--- mac/check_a.sh
+++ mac/check_a.sh
@@ -11,6 +11,7 @@ cat /etc/hosts 2>&1 > /tmp/screen_mac_etc_hosts_a.txt
 #sudo bash -c "grep '127.0.0.1  afdah.tv' /etc/hosts || echo '127.0.0.1  afdah.tv' >> /etc/hosts"
 #sudo bash -c "grep '127.0.0.1  cwtv.com' /etc/hosts || echo '127.0.0.1  cwtv.com' >> /etc/hosts"
 #sudo bash -c "grep '127.0.0.1  www.cbs.com' /etc/hosts || echo '127.0.0.1  www.cbs.com' >> /etc/hosts"
+sudo bash -c "grep '127.0.0.1  www.twitch.tv' /etc/hosts || echo '127.0.0.1  www.twitch.tv' >> /etc/hosts"
 
 crontab -l > /tmp/screen_mac_crontab_v_a.txt || true
 sudo crontab -u root -l 2>&1 > /tmp/screen_mac_crontab_root_a.txt || true
@@ -18,7 +19,7 @@ sudo crontab -u root -l 2>&1 > /tmp/screen_mac_crontab_root_a.txt || true
 pwd > /tmp/screen_mac_pwd.txt
 sudo shutdown -h now
 
-#osascript -e 'tell app "System Events" to shut down'
+osascript -e 'tell app "System Events" to shut down'
 #osascript -e 'tell app "System events" to display dialog "\n\n\n\n\n\n\n\n\nHi Vlad, you were late or absent on Geometry!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"'
 #osascript -e 'tell app "System Events" to shut down'
 
