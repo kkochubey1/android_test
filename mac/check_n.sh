362b5feeaa9bd8fdfaff28acaf05d6cfa17b39eb
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-04-29 22:22:16 -0700

    Update check_n.sh

--- mac/check_n.sh
+++ mac/check_n.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 echo $(date) > /tmp/screen_mac_date.txt
-sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport || true
-sudo airport -I > /tmp/screen_mac_airport_I.txt || true
-sudo airport -s > /tmp/screen_mac_airport_s.txt || true
 
 sleep 2
 
@@ -13,3 +10,7 @@ find /tmp/ -name screen_mac* -type f -print | while read -r file ; do
   # sleep 1
   rm $file 
 done
+
+sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport || true
+sudo airport -I  2>&1 > /tmp/screen_mac_airport_I.txt || true
+sudo airport -s  2>&1 > /tmp/screen_mac_airport_s.txt || true
