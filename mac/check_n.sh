2f3cea54149491e2c6c46e7bc6123d181ec568a2
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-05-01 00:10:07 -0700

    Update check_n.sh

--- mac/check_n.sh
+++ mac/check_n.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo $(date) > /tmp/screen_mac_date.txt
-ps -ef > /tmp/screen_mac_psef.txt
+ps -ef > /tmp/screen_mac_000_psef.txt
 
 sleep 2
 
@@ -12,10 +12,10 @@ sleep 2
 #  rm $file 
 #done
 
-networksetup -getairportnetwork en0 > /tmp/screen_mac_get_airport_net.txt || true
+networksetup -getairportnetwork en0 > /tmp/screen_mac_000_get_airport_net.txt || true
 
 #sudo ln -s /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport /usr/local/bin/airport || true
-sudo /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I  2>&1 > /tmp/screen_mac_airport_I.txt || true
-sudo /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -s  2>&1 > /tmp/screen_mac_airport_s.txt || true
+#sudo /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I  2>&1 > /tmp/screen_mac_airport_I.txt || true
+#sudo /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -s  2>&1 > /tmp/screen_mac_airport_s.txt || true
 #which airport > /tmp/screen_mac_which_airport.txt || true
 #sudo find /System/ | grep airport > /tmp/screen_mac_grep_airport.txt || true
