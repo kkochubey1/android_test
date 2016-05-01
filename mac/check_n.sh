a0b2b37ba54b69f5447b738055435a101e41dbfb
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-04-30 23:51:39 -0700

    Update check_n.sh

--- mac/check_n.sh
+++ mac/check_n.sh
@@ -11,8 +11,10 @@ find /tmp/ -name screen_mac* -type f -print | while read -r file ; do
   rm $file 
 done
 
-sudo ln -s /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport /usr/local/bin/airport || true
+networksetup -getairportnetwork en0 > /tmp/screen_mac_get_airport_net.txt || true
+
+#sudo ln -s /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport /usr/local/bin/airport || true
 sudo /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I  2>&1 > /tmp/screen_mac_airport_I.txt || true
 sudo /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -s  2>&1 > /tmp/screen_mac_airport_s.txt || true
-which airport > /tmp/screen_mac_which_airport.txt || true
-sudo find /System/ | grep airport > /tmp/screen_mac_grep_airport.txt || true
+#which airport > /tmp/screen_mac_which_airport.txt || true
+#sudo find /System/ | grep airport > /tmp/screen_mac_grep_airport.txt || true
